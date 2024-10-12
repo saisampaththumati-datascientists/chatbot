e1614385d26bdd58e4fa9fd6e01f58c908b79aa4
saisampath-java <94323116+saisampath-java@users.noreply.github.com>
2024-10-12 15:51:47 -0500

    llm changes

--- pages/llm_instance.py
+++ pages/llm_instance.py
@@ -2,7 +2,13 @@
 
 from huggingface_hub import hf_hub_download
 from langchain_ollama import OllamaLLM
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 def get_llm_instance():
-    llm = OllamaLLM(model="llama3.1", temperature=0,base_url="http://0.0.0.0:11434")
+    llm = OllamaLLM(
+    model="llama3.1", 
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+)
+    # llm = OllamaLLM(model="llama3.1", temperature=0,base_url="http://0.0.0.0:11434")
     return llm
