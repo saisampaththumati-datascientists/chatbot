a3639133be066f4c9c2f99cc0bcf2d9089f304cd
saisampath-java <94323116+saisampath-java@users.noreply.github.com>
2024-10-12 15:33:40 -0500

    code changes

--- pages/llm_instance.py
+++ pages/llm_instance.py
@@ -1,9 +1,8 @@
 
 
 from huggingface_hub import hf_hub_download
-import ollama
 from langchain_ollama import OllamaLLM
 
 def get_llm_instance():
-    llm = OllamaLLM(model="llama3.1", temperature=0,base_url="http://localhost:11434")
+    llm = OllamaLLM(model="llama3.1", temperature=0,base_url="http://0.0.0.0:11434")
     return llm
