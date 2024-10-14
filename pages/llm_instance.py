4c415eb7ccc76faee046baf359d9d2830c744616
saisampath-java <94323116+saisampath-java@users.noreply.github.com>
2024-10-14 10:33:43 -0500

    changes

--- pages/llm_instance.py
+++ pages/llm_instance.py
@@ -3,8 +3,12 @@
 from huggingface_hub import hf_hub_download
 from langchain_ollama import OllamaLLM
 import requests
-response = requests.get("http://127.0.0.1:11434")
-print(response.text)
+
+try:
+    response = requests.get("http://127.0.0.1:11434")
+    print(response.text)
+except requests.ConnectionError as e:
+    print(f"Connection error: {e}")
 
 def get_llm_instance():
     llm = OllamaLLM(model="llama3.1", temperature=0, base_url="http://127.0.0.1:11434")
