18d6e7d28082a7ed9fe0159fc124f53d1f1329eb
saisampath-java <94323116+saisampath-java@users.noreply.github.com>
2024-10-14 09:53:35 -0500

    deploy changes

--- pages/llm_instance.py
+++ pages/llm_instance.py
@@ -6,6 +6,5 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 def get_llm_instance():
-    llm = OllamaLLM(model="llama3.1", temperature=0,base_url="http://0.0.0.0:11434")
-    # 
+    llm = OllamaLLM(model="llama3.1", temperature=0, base_url="http://127.0.0.1:11434")
     return llm
